55ac7f60b6d2a2e27b6fa43b14135e461e253ba9
Jordan Palmer <jordan@dmpnet.org>
2020-10-13 10:50:46 -0600

    Updated graph

--- HW4.m
+++ HW4.m
@@ -10,10 +10,47 @@ clear
 close all
 
 % Set variables to use for constants
-A = area_data(:,1);
-B = area_data(:,2);
-C = area_data(:,3);
-M = area_data(:,4);
-N = area_data(:,5);
-Z = area_data(:,6);
-W = area_data(:,7);
+N = 10;
+k = 1;
+L = 1;
+U0 = 1;
+v = 1;
+A = 1;
+h = L/(N+1);
+
+% function u = triAlgorithm()
+%     α[1] = a[1] 
+%     g[1] = f[1]
+% 
+%     for j = 2:N
+%         print(j)
+%         α[j] =a[j]− (b[j]/α[j−1])*c[j−1] 
+%         g[j] =f[j]− (b[j]/α[j−1])*g[j−1] 
+%     end
+%     
+%     u[N] =g[N]/α[N] 
+% 
+%     for k = 1:N-1
+%         u[N−k] = (g[N−k]−c[N−k]*u[N−k+1]) / α[N−k]
+%     end 
+% end
+%     
+% 
+% triAlgorithm()
+
+
+% Make plots based of of chosen values
+Plot1 = @(x) (((sinh(k*(L-x))+sinh(k*x))/sinh(k*L))-1)*(A/k^2) + U0*((sinh(k*(L-x)))/sinh(k*L));
+Plot2 = @(x) ((cosh(k*x)/cosh(k*L))-1)*(A/k^2) + U0*(sinh(k*(L-x))/sinh(k*L));
+
+% Plot functions
+fplot(Plot1)
+hold on
+fplot(Plot2)
+
+% Format plot
+xlabel('Variable [x]')
+ylabel('Function [y]')
+title('Test Plot')
+axis([-8,10,-10,10])
+grid on
