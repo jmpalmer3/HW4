13a4d923997500cefa5f5156764be798b3b6a734
Jordan Palmer <jordan@dmpnet.org>
2020-10-13 22:29:45 -0600

    Fixed Neumann function, correct graphing

--- HW4.m
+++ HW4.m
@@ -11,7 +11,7 @@ close all
 
 % Set variables to use for constants
 N = 10;
-k = 1;
+k = 10;
 L = 1;
 U0 = 1;
 v = 1;
@@ -21,7 +21,7 @@ f = A;
 
 
 
-[a, b, c, f, N] = setupNeumann(N, U0, A, L, k);
+[a, b, c, f, N] = setupNeumann(N, v, A, L, k);
 
 % call algorithm function with input values
 u = triAlgorithm(a, b, c, f, N);
@@ -33,7 +33,7 @@ u = triAlgorithm(a, b, c, f, N);
 
 %Make plots based of of chosen values
 %Plot1 = @(x) (((sinh(k*(L-x))+sinh(k*x))/sinh(k*L))-1)*(A/k^2) + U0*((sinh(k*(L-x)))/sinh(k*L));
-Plot1 = @(x) ((cosh(k*x)/cosh(k*L))-1)*(A/k^2) + U0*(sinh(k*(L-x))/sinh(k*L));
+Plot1 = @(x) ((cosh(k*x)/cosh(k*L))-1)*(A/k^2)-(v/k)*(sinh(k*(L-x))/cosh(k*L));
 
 x = (1:length(u))/N;
 
@@ -48,8 +48,8 @@ plot(x,u)
 % Format plot
 xlabel('Length [x]')
 ylabel('Function [u]')
-title(['Dirichlet type [N=',num2str(N),' k=',num2str(k),']'])
-axis([0,1,-1,2])
+title(['Neumann type [N=',num2str(N),' k=',num2str(k),']'])
+axis([0,1,-2,1])
 legend('Theoretical','Computed')
 grid on
 
@@ -57,7 +57,7 @@ grid on
 
 % setup for calculated matrix values
 N=2*N;
-[a, b, c, f, N] = setupNeumann(N, U0, A, L, k);
+[a, b, c, f, N] = setupNeumann(N, v, A, L, k);
 u = triAlgorithm(a, b, c, f, N);
 x = (1:length(u))/N;
 
@@ -69,8 +69,8 @@ plot(x,u)
 % Format plot
 xlabel('Length [x]')
 ylabel('Function [u]')
-title(['Dirichlet type [N=',num2str(N),' k=',num2str(k),']'])
-axis([0,1,-1,2])
+title(['Neumann type [N=',num2str(N),' k=',num2str(k),']'])
+axis([0,1,-2,1])
 legend('Theoretical','Computed')
 grid on
 
@@ -78,7 +78,7 @@ grid on
 
 % setup for calculated matrix values with new N value
 N = 2*N;
-[a, b, c, f, N] = setupNeumann(N, U0, A, L, k);
+[a, b, c, f, N] = setupNeumann(N, v, A, L, k);
 u = triAlgorithm(a, b, c, f, N);
 x = (1:length(u))/N;
 
@@ -90,8 +90,8 @@ plot(x,u)
 % Format plot
 xlabel('Length [x]')
 ylabel('Function [u]')
-title(['Dirichlet type [N=',num2str(N),' k=',num2str(k),']'])
-axis([0,1,-1,2])
+title(['Neumann type [N=',num2str(N),' k=',num2str(k),']'])
+axis([0,1,-2,1])
 legend('Theoretical','Computed')
 grid on
 
@@ -100,7 +100,7 @@ grid on
 
 % setup for calculated matrix values with new N value
 N = 2*N;
-[a, b, c, f, N] = setupNeumann(N, U0, A, L, k);
+[a, b, c, f, N] = setupNeumann(N, v, A, L, k);
 u = triAlgorithm(a, b, c, f, N);
 x = (1:length(u))/N;
 
@@ -112,8 +112,8 @@ plot(x,u)
 % Format plot
 xlabel('Length [x]')
 ylabel('Function [u]')
-title(['Dirichlet type [N=',num2str(N),' k=',num2str(k),']'])
-axis([0,1,-1,2])
+title(['Neumann type [N=',num2str(N),' k=',num2str(k),']'])
+axis([0,1,-2,1])
 legend('Theoretical','Computed')
 grid on
 
--- setupNeumann.m
+++ setupNeumann.m
@@ -1,11 +1,10 @@
-function [a, b, c, f, N] = setupNeumann(N, U0, A, L, k)
+function [a, b, c, f, N] = setupNeumann(N, v, A, L, k)
     f=A;
-    v=1;
     h = L/(N+1);
     a = (-(2+(k^2)*(h^2)))*ones(1,N);
     b = 1*ones(1,N-1);
     c = 1*ones(1,N-1);
     c(1) = 2;
     f = (h^2*f)*ones(1,N);
-    f(1) = f(1)-2*h*v;
+    f(1) = f(1)+2*h*v;
 end
